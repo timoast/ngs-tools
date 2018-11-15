fe291fd6d043a1cb727cc0ed1a11d9885d4e5b98
timoast <timoast@users.noreply.github.com>
2018-11-15 12:27:28 -0500

    check matching chromosome; add more options

--- mean_insert_size.py
+++ mean_insert_size.py
@@ -15,25 +15,28 @@ def get_data(inp):
         else:
             line = line.rsplit()
             length = int(line[8])
-            if length > 0:
+            if length > 0 and line[6] == '=':
                 lengths.append(length)
             else:
                 pass
     return lengths
 
 
-def reject_outliers(data, m=2.):
+def reject_outliers(data, m=2., std=None):
     """
     rejects outliers more than 2
     standard deviations from the median
     """
     median = np.median(data)
-    std = np.std(data)
+    keep = []
+    if std is None:
+        std = np.std(data)
     for item in data:
         if abs(item - median) > m * std:
-            data.remove(item)
-        else:
             pass
+        else:
+            keep.append(item)
+    return keep
 
 
 def calc_size(data):
@@ -44,7 +47,13 @@ def calc_size(data):
 
 if __name__ == "__main__":
     import sys
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser(description="Estimate mean insert size in paired-end reads from BAM file")
+    parser.add_argument("-s", "--std", help="Expected standard deviation", required = False, type=float)
+    parser.add_argument("-l", "--limit", help="Limit for number of standard deviations from median", required = False, type=float, default = 2.)
+    options = parser.parse_args()
     lengths = get_data(sys.stdin)
-    reject_outliers(lengths)
+    lengths = reject_outliers(lengths, std=options.std, m=options.limit)
     mn, std = calc_size(lengths)
     print mn, std
